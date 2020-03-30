1112e106af5069b00eee58f1a81b2ab9ff191dd9
Mauricio <mauricioandrek@hotmail.com>
2020-03-30 19:04:09 +0000

    delet

--- pesquisa/urls.py
+++ pesquisa/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^$', views.index, name='home'),
+    url(r'^leitura/', views.leitura, name='leitura'),
+    url(r'^administrador', views.administrador, name='administrador'),
 ]
