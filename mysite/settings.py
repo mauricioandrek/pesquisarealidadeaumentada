a06e2cd1eaa5a29f60477dca8cd254536eabbdf7
Mauricio <mauricioandrek@hotmail.com>
2020-03-30 15:51:44 -0300

    Adicionado paginas e conteudo

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q81j(8qxy#!0-2!!8)k2tvjyyjr^a@fx@k)ddux&r3r$k*qxo0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
--- pesquisa/views.py
+++ pesquisa/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.files.storage import FileSystemStorage, default_storage
+from django.http import HttpResponse
+from django import template
+import uuid
 
 # Create your views here.
 def index(request):
     return render(request, 'pesquisa/index.html', {})
+
+def leitura(request):
+	return render(request, 'pesquisa/leitura.html', {})
+
+def administrador(request):
+	return render(request, 'pesquisa/admin.html', {})
