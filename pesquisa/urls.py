a477984b26c6f145fe8c30ee5e71f534b82d3fbd
Mauricio <mauricioandrek@hotmail.com>
2020-03-31 12:27:34 -0300

    Ajustes funcionamento

--- pesquisa/urls.py
+++ pesquisa/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from . import views
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
     url(r'^leitura/', views.leitura, name='leitura'),
     url(r'^administrador', views.administrador, name='administrador'),
+    url(r'^ar/', TemplateView.as_view(template_name="pesquisa/ar.html"),
+                   name='ar'),
 ]
