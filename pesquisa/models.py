44b2aace3850808b78981f2c2e015dec93402b3e
Mauricio <mauricioandrek@hotmail.com>
2020-03-31 14:24:05 -0300

    Inserido upload do manifesto através de .csv

--- pesquisa/models.py
+++ pesquisa/models.py
@@ -4,22 +4,22 @@ from django.utils import timezone
 
 
 class Manifest(models.Model):
-    _step = models.TextField()
-    _type = models.TextField()
-    _value = models.TextField()
-    _desc = models.TextField()
-    _imported_date = models.DateTimeField(blank=True, null=True)
+    step = models.TextField()
+    typee = models.TextField()
+    value = models.TextField()
+    desc = models.TextField()
+    imported_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self._imported_date = timezone.now()
         self.save()
 
     def __str__(self):
-        return self._value
+        return self.value
 
     def getType(self):
-        return self._type
+        return self.typee
     def getValue(self):
-        return self._value
+        return self.value
     def getDesc(self):
-        return self._desc
+        return self.desc
--- pesquisa/views.py
+++ pesquisa/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.core.files.storage import FileSystemStorage, default_storage
 from django.http import HttpResponse
 from django import template
+from .models import Manifest
+import csv, io
+from django.utils import timezone
 import uuid
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
@@ -12,4 +19,29 @@ def leitura(request):
 	return render(request, 'pesquisa/leitura.html', {})
 
 def administrador(request):
-	return render(request, 'pesquisa/admin.html', {})
+	global logger
+	dados = Manifest.objects.all()
+	logger.error("dados : {0}".format(dados))  
+	template = 'pesquisa/admin.html'
+	if request.method == "GET":
+		return render(request, template, { 'manifestos': dados })
+	        
+	csv_file = request.FILES['myfile']
+    # checar se é um CSV
+	if not csv_file.name.endswith('.csv'):
+		messages.error(request, 'Não foi selecionado um arquivo CSV')
+		
+	data_set = csv_file.read().decode('UTF-8')
+	io_string = io.StringIO(data_set)
+	next(io_string)
+	Manifest.objects.all().delete()
+	for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+		_, created = Manifest.objects.update_or_create(
+			step=column[0],
+			typee=column[1],
+			value=column[2],
+			desc=column[3],
+			imported_date = timezone.now()
+			)
+	dados = Manifest.objects.all()
+	return render(request, template, {'manifestos' : dados })
